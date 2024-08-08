9033d319359a8b4386fa971abd6c043b6fea1a36
lgk-bsw <lgk@patorg.de>
2024-08-08 14:09:20 +0200

    Remove AutoComplete

--- BlueBlazor/src/all.js
+++ BlueBlazor/src/all.js
@@ -3,7 +3,6 @@
  * Licensed under GNU General Public License v3.0 (https://github.com/bruegmann/blue-blazor/blob/master/LICENSE).
  */
 
-import "./autoComplete.js"
 import "./dialog.js"
 import "./modal.js"
 import "./qrCodeGen.js"
