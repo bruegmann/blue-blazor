6b53b99b933c94408484870eb1a8695844638ac9
lgk-bsw <lgk@patorg.de>
2024-09-05 10:48:44 +0200

    FLOR-1671 Code formatting

--- BlueBlazor/wwwroot/BlueBlazor.lib.module.js
+++ BlueBlazor/wwwroot/BlueBlazor.lib.module.js
@@ -1,82 +1,89 @@
-﻿const pageScriptInfoBySrc = new Map();
+﻿// Based on Code from: https://learn.microsoft.com/en-us/aspnet/core/blazor/javascript-interoperability/static-server-rendering?view=aspnetcore-8.0
+
+const pageScriptInfoBySrc = new Map()
 
 function registerPageScriptElement(src) {
     if (!src) {
-        throw new Error('Must provide a non-empty value for the "src" attribute.');
+        throw new Error(
+            'Must provide a non-empty value for the "src" attribute.'
+        )
     }
 
-    let pageScriptInfo = pageScriptInfoBySrc.get(src);
+    let pageScriptInfo = pageScriptInfoBySrc.get(src)
 
     if (pageScriptInfo) {
-        pageScriptInfo.referenceCount++;
+        pageScriptInfo.referenceCount++
     } else {
-        pageScriptInfo = { referenceCount: 1, module: null };
-        pageScriptInfoBySrc.set(src, pageScriptInfo);
-        initializePageScriptModule(src, pageScriptInfo);
+        pageScriptInfo = { referenceCount: 1, module: null }
+        pageScriptInfoBySrc.set(src, pageScriptInfo)
+        initializePageScriptModule(src, pageScriptInfo)
     }
 }
 
 function unregisterPageScriptElement(src) {
     if (!src) {
-        return;
+        return
     }
 
-    const pageScriptInfo = pageScriptInfoBySrc.get(src);
+    const pageScriptInfo = pageScriptInfoBySrc.get(src)
 
     if (!pageScriptInfo) {
-        return;
+        return
     }
 
-    pageScriptInfo.referenceCount--;
+    pageScriptInfo.referenceCount--
 }
 
 async function initializePageScriptModule(src, pageScriptInfo) {
     if (src.startsWith("./")) {
-        src = new URL(src.substr(2), document.baseURI).toString();
+        src = new URL(src.substr(2), document.baseURI).toString()
     }
 
-    const module = await import(src);
+    const module = await import(src)
 
     if (pageScriptInfo.referenceCount <= 0) {
-        return;
+        return
     }
 
-    pageScriptInfo.module = module;
-    module.onLoad?.();
-    module.onUpdate?.();
+    pageScriptInfo.module = module
+    module.onLoad?.()
+    module.onUpdate?.()
 }
 
 function onEnhancedLoad() {
     for (const [src, { module, referenceCount }] of pageScriptInfoBySrc) {
         if (referenceCount <= 0) {
-            module?.onDispose?.();
-            pageScriptInfoBySrc.delete(src);
+            module?.onDispose?.()
+            pageScriptInfoBySrc.delete(src)
         }
     }
 
     for (const { module } of pageScriptInfoBySrc.values()) {
-        module?.onUpdate?.();
+        module?.onUpdate?.()
     }
 }
 
 export function afterWebStarted(blazor) {
-    customElements.define('page-script', class extends HTMLElement {
-        static observedAttributes = ['src'];
+    customElements.define(
+        "page-script",
+        class extends HTMLElement {
+            static observedAttributes = ["src"]
 
-        attributeChangedCallback(name, oldValue, newValue) {
-            if (name !== 'src') {
-                return;
+            attributeChangedCallback(name, oldValue, newValue) {
+                if (name !== "src") {
+                    return
+                }
+
+                this.src = newValue
+                unregisterPageScriptElement(oldValue)
+                registerPageScriptElement(newValue)
             }
 
-            this.src = newValue;
-            unregisterPageScriptElement(oldValue);
-            registerPageScriptElement(newValue);
+            disconnectedCallback() {
+                unregisterPageScriptElement(this.src)
+            }
         }
+    )
 
-        disconnectedCallback() {
-            unregisterPageScriptElement(this.src);
-        }
-    });
-
-    blazor.addEventListener('enhancedload', onEnhancedLoad);
+    blazor.addEventListener("enhancedload", onEnhancedLoad)
 }
