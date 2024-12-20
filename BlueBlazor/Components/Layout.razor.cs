216bf284194ccd8a8984ab0911d61ab8bdbf8a07
lgk-bsw <lgk@patorg.de>
2024-12-20 09:16:23 +0100

    Layout- und Abhängigkeitsaktualisierungen
    MainLayout.razor: <side-layout> entfernt, <Layout> hinzugefügt mit Slots <HeaderContent>, <SideContent>, <PageContent> für bessere Strukturierung.
    NavMenu.razor: <div> durch <SidebarMenu> ersetzt, Menüeinträge in <TopContent> und <ChildContent> verschoben.
    BasicUsage.razor: Beispiel für grundlegende Nutzung des Layouts hinzugefügt.
    GridLayout.razor: Datei entfernt.
    package-lock.json, package.json: Abhängigkeit `blue-web` von 1.6.4 auf 1.7.0 aktualisiert.

---
+++ BlueBlazor/Components/Layout.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlueBlazor.Components;
+
+/// <summary>
+/// Implementation of Web Component SideLayout by Blue Web.
+/// In some cases, like when using .NET 8's new SSR (Static Server Rendering) rendermode, it might be necessary to include our library script in your App.razor manually. You can do so as follows:
+/// `<script src="_content/BlueBlazor/blue-web/js/side-layout.bundle.js"></script>
+/// </summary>
+public partial class Layout
+{
+    [Inject]
+    private IJSRuntime JSRuntime { get; set; } = default!;
+
+    [Parameter]
+    public RenderFragment? HeaderContent { get; set; }
+
+    [Parameter]
+    public RenderFragment? SideContent { get; set; }
+
+    [Parameter]
+    public RenderFragment? PageContent { get; set; }
+
+    [Parameter]
+    public bool NoPageBorder { get; set; } = false;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            // JS Import especially for "Blazor Server Apps"
+            await JSRuntime.InvokeVoidAsync("import", "./_content/BlueBlazor/blue-web/js/side-layout.bundle.js");
+        }
+    }
+}
