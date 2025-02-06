64c4841269c5db64547ac6592a58090b47d63f4c
lgk-bsw <lgk@patorg.de>
2025-02-06 09:34:17 +0100

    Vereinfachung der SidebarMenu-Komponente
    Die `if`-Bedingungen für `TopContent` und `BottomContent` wurden entfernt. Diese Inhalte werden nun direkt in das Markup eingefügt, unabhängig davon, ob sie null sind oder nicht. Dies führt zu einem kürzeren und einfacheren Code.

---
+++ BlueBlazor/Components/InlineEdit.razor.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlueBlazor.Components;
+
+/**
+ * A wrapper component that allows for inline editing of content.
+ * It starts in read mode and when the user clicks on the content, it switches to edit mode.
+ * State for the edit mode and the event callback have to be passed as a cascading parameter.
+ * Take a look at the example in the docs to see how to use it.
+ */
+public partial class InlineEdit
+{
+    private bool ShowEdit { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public RenderFragment? ReadContent { get; set; }
+
+    [Parameter]
+    public string? ReadText { get; set; }
+
+    [Parameter]
+    public string Id { get; set; } = "InlineEdit-" + Guid.NewGuid().ToString();
+
+    [Parameter]
+    public string? Header { get; set; }
+
+    [CascadingParameter(Name = "EditMode")]
+    protected bool? EditMode { get; set; } = null;
+
+    [CascadingParameter(Name = "EditModeChanged")]
+    protected EventCallback<bool> EditModeChanged { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        if (EditMode == false)
+        {
+            ShowEdit = false;
+        }
+    }
+
+    private void ToggleShowEdit()
+    {
+        ShowEdit = !ShowEdit;
+    }
+
+    private async Task EditRequested()
+    {
+        if (EditMode == false)
+            await EditModeChanged.InvokeAsync(true);
+
+        ShowEdit = true;
+    }
+}
---
+++ BlueBlazor/Components/InlineEditParts/ReadView.razor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlueBlazor.Components.InlineEditParts;
+
+public partial class ReadView
+{
+    [Inject]
+    private IJSRuntime JSRuntime { get; set; } = default!;
+
+    private ElementReference _readViewElement;
+    private IJSObjectReference? _module;
+
+    [Parameter]
+    public EventCallback OnEditRequested { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync("import", "./_content/BlueBlazor/blue-web/js/read-view.bundle.js");
+            _module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/BlueBlazor/Components/InlineEditParts/ReadView.razor.js");
+            await Initialize(_readViewElement);
+        }
+    }
+
+    public async Task Initialize(ElementReference element)
+    {
+        if (_module is not null)
+        {
+            await _module.InvokeVoidAsync("Initialize", element, DotNetObjectReference.Create(this));
+        }
+    }
+
+    [JSInvokable]
+    public async Task Invoke()
+    {
+        await OnEditRequested.InvokeAsync();
+    }
+
+}
