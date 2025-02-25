d9d8362316911b0d1f52a94798567714fc97f2ef
lgk-bsw <lgk@patorg.de>
2025-02-25 15:44:35 +0100

    Deploy

--- docs/_content/BlueBlazor/Components/Layout.razor.js
+++ docs/_content/BlueBlazor/Components/Layout.razor.js
@@ -1,3 +1,19 @@
 ﻿export function onLoad() {
-    import("./_content/BlueBlazor/blue-web/js/side-layout.bundle.js")
+    const expandEl = document.getElementById("layout-expand")
+    if (!expandEl) return
+
+    const entry = localStorage.getItem("side-layout-shrink")
+    const shrink = entry != null
+    expandEl.checked = !shrink
+
+    expandEl.addEventListener("change", handleChange)
+}
+
+function handleChange() {
+    if (localStorage.getItem("side-layout-shrink")) {
+        localStorage.removeItem("side-layout-shrink")
+    }
+    else {
+        localStorage.setItem("side-layout-shrink", "true")
+    }
 }
