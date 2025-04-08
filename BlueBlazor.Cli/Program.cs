964bf7618ebd7f5e72108c8423cf9c290ef3ef6b
lgk-bsw <lgk@patorg.de>
2025-04-08 14:09:41 +0200

    BLUE-18 With help of ChatGPT created CLI

---
+++ BlueBlazor.Cli/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.CommandLine;
+using BlueBlazor.Cli.Commands;
+
+var rootCommand = new RootCommand("BlueBlazor CLI Tool")
+{
+    AddCommand.Create(),
+    ListCommand.Create()
+};
+
+return await rootCommand.InvokeAsync(args);
