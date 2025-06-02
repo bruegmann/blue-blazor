1d09651077c3fb497155fdf87184ff4df44e4df2
lgk-bsw <lgk@patorg.de>
2025-06-02 10:17:34 +0200

    BLUE-21 Tabs improved
    - Refactored index.html for better readability and organization of head elements.
    - Updated prism.css to version 1.30.0, enhancing code highlighting styles.
    - Modified Tab.razor and Tab.razor.cs to include unique IDs for tabs and tab panels, improving accessibility.
    - Adjusted Tabs.razor and Tabs.razor.cs to support dynamic styling and added Underline property for tab styles.
    - Updated package-lock.json and package.json to use blue-web version 1.14.3 and bootstrap version 5.3.6.

--- BlueBlazor/Components/Tab.razor.cs
+++ BlueBlazor/Components/Tab.razor.cs
@@ -1,8 +1,38 @@
-﻿namespace BlueBlazor.Components;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlueBlazor.Components;
 
 /// <summary>
 /// Always use together with `Tabs`.
 /// </summary>
 public partial class Tab
 {
+    private string _tabId = "Tab_" + Guid.NewGuid().ToString();
+    private string _tabPanelId = "Tab_Panel_" + Guid.NewGuid().ToString();
+
+    [Parameter, EditorRequired]
+    public required string Label { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public string? Class { get; set; }
+
+    [Parameter]
+    public string? ContentClass { get; set; }
+
+    [Parameter]
+    public bool Active { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+
+    [CascadingParameter(Name = "Tab_Name")]
+    protected string? Name { get; set; }
 }
--- BlueBlazor/Components/Tabs.razor.cs
+++ BlueBlazor/Components/Tabs.razor.cs
@@ -1,4 +1,6 @@
-﻿namespace BlueBlazor.Components;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlueBlazor.Components;
 
 /// <summary>
 /// Implementation of Blue Web Tabs. Allows to use tabs without JavaScript.
@@ -6,4 +8,25 @@
 /// </summary>
 public partial class Tabs
 {
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// Will be passed down to `Tab` children as identifier. Default is `"TabList-" + Guid.NewGuid().ToString()`.
+    /// </summary>
+    [Parameter]
+    public string? Name { get; set; } = "TabList-" + Guid.NewGuid().ToString();
+
+    [Parameter]
+    public string? Class { get; set; }
+
+    [Parameter]
+    public string? Style { get; set; }
+
+    /// <summary>
+    /// By default, tabs will have styling of Bootstrap `.nav.nav-tabs`.
+    /// Set this property to `true` to use `.nav.nav-underline` instead.
+    /// </summary>
+    [Parameter]
+    public bool Underline { get; set; } = false;
 }
