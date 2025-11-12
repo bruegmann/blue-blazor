c80d78f4ade04f9919baf0d7fca017bb2cfb907c
lgk-bsw <lgk@patorg.de>
2025-11-12 10:52:24 +0100

    Refactor NavMenu to partial class and move logic to code-behind
    Moved NavMenu component logic from NavMenu.razor to a new NavMenu.razor.cs code-behind file for better separation of concerns and maintainability. Updated method names and references in the .razor file. Also added additional compiler warnings to the project file.

---
+++ BlueBlazor.Docs/Components/Layouts/NavMenu.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using System.Text.Json;
+
+namespace BlueBlazor.Docs.Components.Layouts;
+
+public partial class NavMenu : ComponentBase
+{
+    [Inject]
+    private HttpClient Http { get; set; } = default!;
+
+    [Inject]
+    private NavigationManager NavigationManager { get; set; } = default!;
+
+    public void Dispose()
+    {
+        NavigationManager.LocationChanged -= HandleLocationChanged;
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        NavigationManager.LocationChanged += HandleLocationChanged;
+        await LoadPagesData();
+    }
+
+    private void HandleLocationChanged(object? sender, LocationChangedEventArgs e)
+    {
+        StateHasChanged();
+    }
+
+    private bool IsActive(string href, bool exact = false)
+    {
+        Uri uri = NavigationManager.ToAbsoluteUri(href);
+        string uriString = uri.ToString();
+
+        if (exact ? (NavigationManager.Uri == uriString) : (NavigationManager.Uri.StartsWith(uriString)))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private async Task LoadPagesData()
+    {
+        var json = await Http.GetStringAsync("data/pages-data.json?v=7");
+        componentPageUrls = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(json);
+    }
+
+    Dictionary<string, Dictionary<string, string>>? componentPageUrls;
+}
