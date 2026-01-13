f8cd6460518ab0c9feba2d22466685bdabd9ed71
lgk-bsw <lgk@patorg.de>
2026-01-13 10:30:02 +0100

    Updates

--- _content/BlueBlazor/blue-web/js/odometer.d.ts
+++ _content/BlueBlazor/blue-web/js/odometer.d.ts
@@ -1,6 +1,16 @@
+/**
+ * Odometer web component for animated number display
+ *
+ * @element bl-odometer
+ * @attr {number} value - Current value to display
+ * @attr {number} max - Maximum value before showing "+"
+ * @fires change - Fired when value changes
+ * @cssprop --bl-odometer-duration - Transition duration for number animation (default: 0.4s)
+ */
 declare class Odometer extends HTMLElement {
     private wrapper;
     private _initialized;
+    private _resetTimer;
     static get observedAttributes(): string[];
     constructor();
     connectedCallback(): void;
@@ -8,6 +18,9 @@ declare class Odometer extends HTMLElement {
     private rebuildNumbers;
     attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null): void;
     private updateValue;
+    private clearResetTimer;
+    private scheduleResetChange;
+    private applyChangeState;
     set value(value: number);
     get value(): number;
     set max(value: number);
--- _content/BlueBlazor/blue-web/js/odometer.js
+++ _content/BlueBlazor/blue-web/js/odometer.js
@@ -1,6 +1,15 @@
 function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == typeof i ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != typeof i) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+/**
+ * Odometer web component for animated number display
+ *
+ * @element bl-odometer
+ * @attr {number} value - Current value to display
+ * @attr {number} max - Maximum value before showing "+"
+ * @fires change - Fired when value changes
+ * @cssprop --bl-odometer-duration - Transition duration for number animation (default: 0.4s)
+ */
 class Odometer extends HTMLElement {
   static get observedAttributes() {
     return ["value", "max"];
@@ -8,6 +17,7 @@ class Odometer extends HTMLElement {
   constructor() {
     super();
     _defineProperty(this, "_initialized", false);
+    _defineProperty(this, "_resetTimer", null);
     this.attachShadow({
       mode: "open"
     });
@@ -22,7 +32,7 @@ class Odometer extends HTMLElement {
   }
   setup() {
     const style = document.createElement("style");
-    style.textContent = /* CSS */"\n        :host {\n          display: inline-flex;\n          min-width: 1em;\n          height: 1em;\n          text-align: center;\n          overflow: hidden;\n          line-height: 1em;\n          transition: background-color 0.3s;\n        }\n        .numbers {\n          display: flex;\n          flex-direction: column;\n          transition: transform 0.4s cubic-bezier(0.34, 1.56, 0.64, 1);\n        }\n        .numbers span {\n          display: flex;\n          align-items: center;\n          justify-content: center;\n          height: 1em;\n        }";
+    style.textContent = /* CSS */"\n        :host {\n          display: inline-flex;\n          min-width: 1em;\n          height: 1em;\n          text-align: center;\n          overflow: hidden;\n          line-height: 1em;\n        }\n        .numbers {\n          display: flex;\n          flex-direction: column;\n          transition: transform var(--bl-odometer-duration, 0.4s) cubic-bezier(0.34, 1.56, 0.64, 1);\n        }\n        .numbers span {\n          display: flex;\n          align-items: center;\n          justify-content: center;\n          height: 1em;\n        }";
     const wrapper = document.createElement("span");
     wrapper.classList.add("numbers");
     wrapper.setAttribute("aria-hidden", "true");
@@ -50,7 +60,10 @@ class Odometer extends HTMLElement {
   attributeChangedCallback(name, oldValue, newValue) {
     if (newValue !== oldValue && this._initialized) {
       if (name === "value") {
-        this.updateValue(parseInt(newValue !== null && newValue !== void 0 ? newValue : "0", 10));
+        const prev = parseInt(oldValue !== null && oldValue !== void 0 ? oldValue : "0", 10) || 0;
+        const next = parseInt(newValue !== null && newValue !== void 0 ? newValue : "0", 10) || 0;
+        this.applyChangeState(prev, next);
+        this.updateValue(next);
       } else if (name === "max") {
         this.rebuildNumbers();
       }
@@ -64,12 +77,34 @@ class Odometer extends HTMLElement {
     const displayValue = value > max ? "".concat(max, "+") : "".concat(value);
     this.setAttribute("aria-label", displayValue);
   }
+  clearResetTimer() {
+    if (this._resetTimer !== null) {
+      clearTimeout(this._resetTimer);
+      this._resetTimer = null;
+    }
+  }
+  scheduleResetChange() {
+    this.clearResetTimer();
+    const delay = 3000;
+    this._resetTimer = window.setTimeout(() => {
+      this.removeAttribute("data-change");
+      this._resetTimer = null;
+    }, delay);
+  }
+  applyChangeState(previous, next) {
+    if (next > previous) {
+      this.setAttribute("data-change", "up");
+      this.scheduleResetChange();
+    } else if (next < previous) {
+      this.setAttribute("data-change", "down");
+      this.scheduleResetChange();
+    }
+  }
   set value(value) {
     const current = this.value;
     if (current !== value) {
       this.setAttribute("value", String(value));
       if (this._initialized) {
-        this.updateValue(value);
         this.dispatchEvent(new CustomEvent("change", {
           detail: {
             value
