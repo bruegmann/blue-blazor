9fb266761760ad8a60ef2b5eaa2643996a550d33
[nku-bsw] <nku@patorg.de>
2026-04-15 11:17:15 +0200

    FLOR-2123 Add InlineEditProvider

--- BlueBlazor/Components/InlineEdit/InlineEdit.razor.cs
+++ BlueBlazor/Components/InlineEdit/InlineEdit.razor.cs
@@ -30,6 +30,8 @@ public partial class InlineEdit : BlueComponentBase
     private string? HeaderClassValue => new CssBuilder("blue-label")
         .AddClass("visually-hidden", HeaderHidden).Build();
 
+    private bool ShowEditValue => Provider != null && Provider.ActiveInlineEdit == this ? true : Provider == null ? ShowEdit : false;
+
     /// <summary>
     /// Content for edit mode.
     /// </summary>
@@ -108,6 +110,9 @@ public partial class InlineEdit : BlueComponentBase
     [CascadingParameter(Name = "ControlId")]
     protected string? ControlId { get; set; }
 
+    [CascadingParameter]
+    private InlineEditProvider? Provider { get; set; }
+
     protected override void OnParametersSet()
     {
         if (EditMode == false)
@@ -119,6 +124,7 @@ public partial class InlineEdit : BlueComponentBase
     private void ToggleShowEdit()
     {
         ShowEdit = !ShowEdit;
+        Provider?.ToggleActiveInlineEdit(this);
     }
 
     private async Task EditRequested()
@@ -128,6 +134,8 @@ public partial class InlineEdit : BlueComponentBase
             await EditModeChanged.InvokeAsync(true);
         }
         ShowEdit = true;
+
+        Provider?.ToggleActiveInlineEdit(this);
     }
 
     private void OnClickSubmitButton()
---
+++ BlueBlazor/Components/InlineEdit/InlineEditProvider.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+namespace BlueBlazor.Components;
+
+public partial class InlineEditProvider
+{
+    internal InlineEdit? ActiveInlineEdit { get; set; } = null!;
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    internal void ToggleActiveInlineEdit(InlineEdit inlineEdit)
+    {
+        if (ActiveInlineEdit != inlineEdit)
+        {
+            ActiveInlineEdit = inlineEdit;
+            StateHasChanged();
+        }else if(ActiveInlineEdit == inlineEdit)
+        {
+            ActiveInlineEdit = null;
+            StateHasChanged();
+        }
+    }
+}
